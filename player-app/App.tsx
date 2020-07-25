156d61c5d70ea496e48ffda455f03c9801984296
dk <dk@dk.com>
2020-07-25 14:48:54 -0400

    clearer app

--- player-app/App.tsx
+++ player-app/App.tsx
@@ -5,16 +5,16 @@ import { Image, Button, StyleSheet, Text, TextInput, View } from 'react-native';
 export interface PlayerProps {
   name: string;
   address: string;
-  picture_url: string;
+  pictureUrl: string;
 }
 
 const Player: React.FC<PlayerProps> = (props) => {
   return (
-    <View>
-      <View>
+    <View style={styles.player}>
+      <View style={{ alignItems: "center" }}>
         <Image
-          style={styles.player_image}
-          source={props.picture_url}
+          style={styles.playerImage}
+          source={props.pictureUrl}
         />
       </View>
       <View>
@@ -32,15 +32,37 @@ const Pot: React.FC<PotProps> = (props) => {
   const [value, onChangeText] = React.useState('0');
 
   return (
-    <View>
-      <TextInput
-        style={{ height: 40, borderColor: 'grey', borderWidth: 1 }}
-        onChangeText={text => onChangeText(text)}
-        value={value}
-      />     
+    <View style={styles.pot}>
+      <Text style={{ color:"gold", fontSize:20 }}>Pot</Text>
+      <View style={{ alignItems: "center", justifyContent: "center"}}>
+        <Image
+          style={{ width:154, height:90 }}
+          source="http://petcaretips.net/daffyduck2.jpg"
+        />
+        <TextInput
+          onChangeText={text => onChangeText(text)}
+          value={value}
+          style={{ color: "gold", borderWidth: 1, borderColor: "gold", width: 100, margin: 10, }}
+        />     
+      </View>
     </View>
   );
 } 
+
+export interface PayoutScriptProps {
+  script: string;
+}
+
+const PayoutScript: React.FC<PayoutScriptProps> = (props) => {
+  return (
+    <View style={styles.payoutScript}>
+      <Text style={{ color:"lime", fontSize:20 }}>Payouts</Text>
+      <View style={{ borderColor: "lime", borderWidth: 1, flex: 1 }}>
+        <Text style={{ padding: 5 }}>{props.script}</Text>
+      </View>
+    </View>
+  );
+}
 
 const Terms = (props) => {
   return (
@@ -68,33 +90,20 @@ const Arbitration = (props) => {
 
 const Arbiter: React.FC<PlayerProps> = (props) => {
   return (
-    <View>
-      <View>
+    <View style={{ borderWidth: 1}}>
+      <Text style={{ padding: 1, fontSize: 16 }}>Arbiter</Text>
+      <View style={{ alignItems: "center" }}>
         <Image
-          style={styles.arbiter_image}
-          source={props.picture_url}
+          style={styles.arbiterImage}
+          source={props.pictureUrl}
         />
       </View>
       <View>
         <Text>{props.name}</Text>
       </View>
-      <Text>arbiter</Text>
     </View>
   );
 } 
-
-export interface PayoutScriptProps {
-  script: string;
-}
-
-const PayoutScript: React.FC<PayoutScriptProps> = (props) => {
-  return (
-    <View>
-      <Text>{props.script}</Text>
-    </View>
-  );
-}
-  
 
 export default function App() {
   return (
@@ -104,12 +113,17 @@ export default function App() {
           <PayoutScript script=" winner takes all" />
         </Terms>
         <Players>
-          <Player name="Akin Toulouse" picture_url="https://static-cdn.jtvnw.net/emoticons/v1/425618/1.0"/>
-          <Text> vs. </Text>
-          <Player name="Betsy Wildly" picture_url="https://static-cdn.jtvnw.net/emoticons/v1/30259/1.0"/>
+          <Player name="Akin Toulouse" pictureUrl="https://static-cdn.jtvnw.net/emoticons/v1/425618/1.0"/>
+          <View style={{ alignItems: "center", justifyContent: "center" }}>
+            <Image
+              style={{ width: 25, height: 21, }}
+              source="https://i.imgur.com/riXXKnJ.png"
+            />
+          </View>
+          <Player name="Betsy Wildly" pictureUrl="https://static-cdn.jtvnw.net/emoticons/v1/30259/1.0"/>
         </Players>
         <Arbitration>
-          <Arbiter name="Gordon Blue" picture_url="https://static-cdn.jtvnw.net/emoticons/v1/28/1.0" />
+          <Arbiter name="Gordon Blue" pictureUrl="https://static-cdn.jtvnw.net/emoticons/v1/28/1.0" />
         </Arbitration>
       </View>
   );
@@ -118,7 +132,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'grey',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -130,11 +144,33 @@ const styles = StyleSheet.create({
   },
   arbitration: {
   },
-  player_image: {
+  player: {
+    padding: 10,
+  },
+  pot: {
+    width: 200, 
+    height: 200, 
+    borderWidth: 2, 
+    padding: 10,
+    margin: 2,
+  },
+  payoutScript: {
+    width: 200, 
+    height: 200, 
+    borderWidth: 2, 
+    padding: 10,
+    margin: 2,
+    //    alignItems: "center", 
+    //    justifyContent: "center"
+  },
+  arbitration: {
+    padding: 10,
+  },
+  playerImage: {
     width: 28,
     height: 28,
   },
-  arbiter_image: {
+  arbiterImage: {
     width: 39,
     height: 27,
   },
