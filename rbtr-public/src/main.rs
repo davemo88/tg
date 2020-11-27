59e7aeb0b8ed6a66cab4d67a2e467ef97e1e87f0
d <d@kasofsk.xyz>
2020-11-26 22:10:09 -0500

    redis dont work

--- rbtr-public/src/main.rs
+++ rbtr-public/src/main.rs
@@ -4,10 +4,6 @@ use std::{
         Arc,
     },
 };
-use warp::{
-    Filter,
-};
-use hex::decode;
 use bdk::{
     bitcoin::{
         Address,
@@ -20,6 +16,20 @@ use bdk::{
     },
 };
 use bip39::Mnemonic;
+use hex::decode;
+use redis::{
+    self,
+    Commands,
+};
+use tokio::{
+    sync::{
+        Mutex,
+        RwLock
+    }
+};
+use warp::{
+    Filter,
+};
 use tglib::{
     contract::Contract,
     payout::Payout,
@@ -32,14 +42,22 @@ use tglib::{
         ARBITER_FINGERPRINT,
         ARBITER_XPUBKEY,
         NETWORK,
+        REDIS_SERVER,
     },
 };
 
 mod wallet;
 use wallet::Wallet;
 
+type RedisClient = Arc<RwLock<redis::Client>>;
+
 fn wallet() -> Wallet {
     Wallet::new(Fingerprint::from_str(ARBITER_FINGERPRINT).unwrap(), ExtendedPubKey::from_str(ARBITER_XPUBKEY).unwrap(), NETWORK)
+}
+
+async fn push_contract(client: RedisClient, contract: &Contract, hex: String) {
+    let mut conn: redis::Connection = client.read().await.get_connection().unwrap();
+    let _: () = conn.set(hex::encode(contract.cxid()), hex).unwrap();
 }
 
 #[tokio::main]
@@ -48,6 +66,8 @@ async fn main() {
     let escrow_pubkey = wallet().get_escrow_pubkey();
     let fee_address = warp::any().map(move || Address::p2wpkh(&escrow_pubkey, NETWORK).unwrap());
     let escrow_pubkey = warp::any().map(move || escrow_pubkey.clone());
+    let client = Arc::from(RwLock::from(redis::Client::open(REDIS_SERVER).unwrap()));
+    let redis = warp::any().map(move || client.clone());
 
     let get_escrow_pubkey = warp::path("escrow-pubkey")
         .and(escrow_pubkey)
@@ -59,11 +79,15 @@ async fn main() {
 
     let submit_contract = warp::path("submit-contract")
         .and(warp::path::param::<String>())
-        .map(|contract_hex| {
-            match Contract::from_bytes(hex::decode(contract_hex).unwrap()) {
+        .and(redis)
+        .map(|contract_hex: String, r: RedisClient| {
+            match Contract::from_bytes(hex::decode(contract_hex.clone()).unwrap()) {
                 Ok(c) => {
                     match wallet().validate_contract(&c) {
-                        Ok(_) => format!("contract: {:?}", c),
+                        Ok(()) => {
+                            push_contract(r.clone(), &c.clone(), contract_hex).await;
+                            format!("contract {:?} submitted", c.cxid())
+                        }
                         Err(e) => format!("err: {:?}", e)
                     }
                 },
