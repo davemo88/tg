58330d7ffe7fdf9950d4c7ab0ebd09dec8cb1c6c
d <d@kasofsk.xyz>
2020-11-29 20:46:31 -0500

    rbtr private lil cleanup

--- rbtr-private/src/main.rs
+++ rbtr-private/src/main.rs
@@ -46,19 +46,16 @@ async fn main() {
     let redis_client = redis::Client::open(REDIS_SERVER).unwrap();
     let mut con = redis_client.get_async_connection().await.unwrap();
     loop {
-        let contract_hex: redis::RedisResult<String> = con.lpop("contracts").await;
-        if contract_hex.is_ok() {
-            let contract_hex = contract_hex.unwrap();
+        let r: redis::RedisResult<String> = con.lpop("contracts").await;
+        if let Ok(contract_hex) = r {
             let contract = Contract::from_bytes(hex::decode(contract_hex).unwrap()).unwrap();
             let wallet = Wallet::new();
-            println!("retreived contract:\n{:?}", contract);
+            println!("retrieved contract:\n{:?}", contract);
             if wallet.validate_contract(&contract).is_ok() {
                 let sig = wallet.sign_message(Message::from_slice(&contract.cxid()).unwrap(), 
                     DerivationPath::from_str(&format!("m/{}/{}", ESCROW_SUBACCOUNT, ESCROW_KIX)).unwrap()).unwrap();
-                let r: redis::RedisResult<String> = con.set(hex::encode(contract.cxid()), sig.to_string()).await;
-                println!("signed {} with sig {} and pushed to redis", hex::encode(contract.cxid()), sig.to_string());
-                let r: redis::RedisResult<String> = con.get(hex::encode(contract.cxid())).await;
-                println!("used key {} to retrieve sig {} from redis", hex::encode(contract.cxid()), sig.to_string());
+                let _: redis::RedisResult<String> = con.set(hex::encode(contract.cxid()), sig.to_string()).await;
+                println!("signed contract {} and pushed sig to redis", hex::encode(contract.cxid()));
             }
         }
         sleep(Duration::from_secs(5));
