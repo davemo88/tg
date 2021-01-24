4644143960e5e3445037d19619cbe79614882eb3
d <d@kasofsk.xyz>
2021-01-24 00:30:46 -0500

    move const

--- PlayerApp/rust/src/lib.rs
+++ PlayerApp/rust/src/lib.rs
@@ -6,14 +6,14 @@ use jni::{
     },
     sys::jstring,
 };
-
 use libcli;
+
+const LOCALHOST: &'static str  = "10.0.2.2";
 
 #[no_mangle]
 pub unsafe extern "system" fn Java_com_playerapp_PlayerWalletModule_cli(env: JNIEnv, _: JClass, command: JString) -> jstring { 
     let command: String = env.get_string(command).unwrap().into();
 // 10.0.2.2 is for testing in the android studio emulator
-    let LOCALHOST = "10.0.2.2";
     let conf = libcli::Conf {
         electrs_url: format!("tcp://{}:60401", LOCALHOST),
         name_url: format!("http://{}:18420", LOCALHOST),
