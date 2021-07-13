861aae12a96a838c982a9c7325176f1825d6f93c
davemo88 <david@kasofsk.xyz>
2021-07-12 22:51:07 -0400

    slightly better errors

--- player-wallet/src/exchange.rs
+++ player-wallet/src/exchange.rs
@@ -23,6 +23,10 @@ use tglib::{
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 type Response = JsonResponse<()>;
 
+// TODO: maybe these errors could be handled better
+const MISSING_DATA_MSG: &'static str = "missing data";
+const UNKNOWN_ERROR_MSG: &'static str = "unknown exchange error";
+
 pub struct ExchangeClient(String);
 
 impl ExchangeClient {
@@ -57,23 +61,17 @@ impl ExchangeService for ExchangeClient {
         let response: Response = serde_json::from_str(&response.text()?)?;
         match response.status {
             Status::Success => Ok(()),
-            Status::Error => Err(Error::JsonResponse(response.message.unwrap_or("unknown exchange error".into())).into())
+            Status::Error => Err(Error::JsonResponse(response.message.unwrap_or(UNKNOWN_ERROR_MSG.into())).into())
         }
-//        Ok(())
     }
 
     fn get_contract_info(&self, player_name: PlayerName) -> Result<Option<PlayerContractInfo>> {
         let response = self.get("get-contract-info", Some(&hex::encode(player_name.0.as_bytes())))?;
         let response: JsonResponse<String> = serde_json::from_str(&response.text()?)?;
         match response.status {
-            Status::Success => Ok(Some(serde_json::from_str(&response.data.unwrap()).unwrap())),
-            Status::Error => Err(Error::JsonResponse(response.message.unwrap_or("unknown exchange error".into())).into())
+            Status::Success => Ok(Some(serde_json::from_str(&response.data.ok_or(Error::Adhoc(MISSING_DATA_MSG))?)?)),
+            Status::Error => Err(Error::JsonResponse(response.message.unwrap_or(UNKNOWN_ERROR_MSG.into())).into())
         }
-//        let contract_info = match serde_json::from_str::<PlayerContractInfo>(&response.text().unwrap()) {
-//            Ok(info) => Some(info),
-//            Err(_) => None,
-//        };
-//        Ok(contract_info)
     }
 
     fn send_contract(&self, contract: TokenContractRecord, player_name: PlayerName) -> Result<()> {
@@ -81,12 +79,11 @@ impl ExchangeService for ExchangeClient {
             contract,
             player_name,
         };
-        let response = self.post("send-contract", serde_json::to_string(&body).unwrap())?;
-//        Ok(())
+        let response = self.post("send-contract", serde_json::to_string(&body)?)?;
         let response: Response = serde_json::from_str(&response.text()?)?;
         match response.status {
             Status::Success => Ok(()),
-            Status::Error => Err(Error::JsonResponse(response.message.unwrap_or("unknown exchange error".into())).into())
+            Status::Error => Err(Error::JsonResponse(response.message.unwrap_or(UNKNOWN_ERROR_MSG.into())).into())
         }
     }
 
@@ -95,12 +92,11 @@ impl ExchangeService for ExchangeClient {
             payout,
             player_name,
         };
-        let response = self.post("send-payout", serde_json::to_string(&body).unwrap())?; 
-//        Ok(())
+        let response = self.post("send-payout", serde_json::to_string(&body)?)?; 
         let response: Response = serde_json::from_str(&response.text()?)?;
         match response.status {
             Status::Success => Ok(()),
-            Status::Error => Err(Error::JsonResponse(response.message.unwrap_or("unknown exchange error".into())).into())
+            Status::Error => Err(Error::JsonResponse(response.message.unwrap_or(UNKNOWN_ERROR_MSG.into())).into())
         }
     }
 
@@ -108,30 +104,26 @@ impl ExchangeService for ExchangeClient {
         let response = self.get("auth-token", Some(&player_name.0))?; 
         let response: JsonResponse<String> = serde_json::from_str(&response.text()?)?;
         match response.status {
-            Status::Success => Ok(hex::decode(response.data.unwrap())?),
-            Status::Error => Err(Error::JsonResponse(response.message.unwrap_or("unknown exchange error".into())).into())
+            Status::Success => Ok(hex::decode(response.data.ok_or(Error::Adhoc(MISSING_DATA_MSG))?)?),
+            Status::Error => Err(Error::JsonResponse(response.message.unwrap_or(UNKNOWN_ERROR_MSG.into())).into())
         }
-//        let token = hex::decode(response.text()?)?.to_vec();
-//        Ok(token)
     }
 
     fn receive_contract(&self, auth: AuthTokenSig) -> Result<Option<TokenContractRecord>> {
         let response = self.post("receive-contract", serde_json::to_string(&auth)?)?; 
-//        Ok(serde_json::from_str::<TokenContractRecord>(&response.text()?).ok())
         let response: JsonResponse<String> = serde_json::from_str(&response.text()?)?;
         match response.status {
-            Status::Success => Ok(Some(serde_json::from_str::<TokenContractRecord>(&response.data.unwrap())?)),
-            Status::Error => Err(Error::JsonResponse(response.message.unwrap_or("unknown exchange error".into())).into())
+            Status::Success => Ok(Some(serde_json::from_str::<TokenContractRecord>(&response.data.ok_or(Error::Adhoc(MISSING_DATA_MSG))?)?)),
+            Status::Error => Err(Error::JsonResponse(response.message.unwrap_or(UNKNOWN_ERROR_MSG.into())).into())
         }
     }
 
     fn receive_payout(&self, auth: AuthTokenSig) -> Result<Option<PayoutRecord>> {
         let response = self.post("receive-payout", serde_json::to_string(&auth)?)?;
-//        Ok(serde_json::from_str::<PayoutRecord>(&response.text()?).ok())
         let response: JsonResponse<String> = serde_json::from_str(&response.text()?)?;
         match response.status {
-            Status::Success => Ok(Some(serde_json::from_str::<PayoutRecord>(&response.data.unwrap())?)),
-            Status::Error => Err(Error::JsonResponse(response.message.unwrap_or("unknown exchange error".into())).into())
+            Status::Success => Ok(Some(serde_json::from_str::<PayoutRecord>(&response.data.ok_or(Error::Adhoc(MISSING_DATA_MSG))?)?)),
+            Status::Error => Err(Error::JsonResponse(response.message.unwrap_or(UNKNOWN_ERROR_MSG.into())).into())
         }
     }
 
